5f4e6a9a4ae4adfce9c7069dc54e46c9beb9ce43
gokulprasad <hi2gokul13@gmail.com>
2025-05-31 16:30:20 +0530

    Add files via upload

---
+++ dsa.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class oma{
+    public static void main(String[] args){
+        Queue<String> omen=new LinkedList<String>();
+        
+        omen.offer("karen");
+        omen.offer("hello");
+        
+        System.out.println(omen);
+        
+    }
+}
